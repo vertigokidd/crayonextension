4324fd3866fa83dae2ac82c30b9df9fa4643885e
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 10:27:57 -0500

    changes name of paperscript.js to drawController.js, and adds ability to access json string for drawing in contentscript.js (may move this functionality to drawController.js)

---
+++ drawController.js
@@ -0,0 +1,53 @@
+var myPath;
+var color = 'blue';
+var strokeCap = 'round';
+var width = 5;
+var opacity = 1;
+myProject = project
+
+function onMouseDown(event) {
+  myPath = new Path();
+  myPath.strokeColor = color;
+  myPath.strokeWidth = width;
+  myPath.strokeCap = strokeCap;
+  myPath.opacity = opacity;
+
+};
+
+function onMouseDrag(event) {
+  myPath.add(event.point);
+  myPath.smooth();
+};
+
+$('.marker').on('mouseup', function(){
+  color = $('#color').val();
+});
+
+$('.marker').on('mouseleave', function(){
+  color = $('#color').val();
+});
+
+$('#width').change(function() {
+  var newWidth = $(this).val();
+  width = parseInt(newWidth);
+  $('#current_width').html(width);
+});
+
+$('#opacity').change(function() {
+  var newOpacity = $(this).val();
+  opacity = parseFloat(newOpacity)/100;
+  $('#current_opacity').html(newOpacity + "%");
+});
+
+$('#gyc-undo-button').click(function() {
+  myPath.remove();
+});
+
+var farbtasticWheel = chrome.extension.getURL("wheel.png");
+var farbtasticMask = chrome.extension.getURL("mask.png");
+var farbtasticMarker = chrome.extension.getURL("marker.png");
+
+$('.farbtastic .wheel').css("background", "url('" + farbtasticWheel + "') no-repeat");
+$('.farbtastic .overlay').css("background", "url('" + farbtasticMask + "') no-repeat");
+$('.farbtastic .marker').css("background", "url('" + farbtasticMarker + "') no-repeat");
+
