7534803ad4ab68b02418ea4d9fc925d8fab9c51c
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-29 13:47:16 -0500

    cleaned contentscript js

--- contentscript.js
+++ contentscript.js
@@ -1,58 +1,67 @@
-// var s = document.createElement('script');
-// s.src = chrome.extension.getURL('gycscript.js');
-// s.type = "text/paperscript";
-// $(s).attr("canvas", "myCanvas");
-// (document.body||document.documentElement).appendChild(s);
-// s.onload = function() {
-//     s.parentNode.removeChild(s);
-// };
+// Calls all initialize methods
+injectScripts();
+injectToolbar();
+initializeMessageListener();
+initializeAccordion();
+initializeDraggable();
+initializeColorPicker();
 
 
-$('body').append('<div class="getyourcrayon-menubar"><div id="toolbar-header"></div><div id="toolbar"><div id="toolbar-toggle"><p id="toggle-toolbar-arrow">&#9660</p></div><div id="toolbar-tools"><button type="gyc-button" id="gyc-paint-button" class="gyc-btn gyc-btn-default">Paint</button><button type="gyc-button" id="gyc-undo-button" class="gyc-btn gyc-btn-default">Undo</button><button type="gyc-button" id="gyc-save-button" class="gyc-btn gyc-btn-default">Save</button><label>Width: <span id="current_width">5</span><br><input id="width" type="range" name="points" min="1" max="40" value="5"></label><br><label>Opacity: <span id="current_opacity">100%</span><br><input id="opacity" type="range" name="points" min="1" max="100" value="100"></label><form><input type="text" id="color" name="color" value="#123456" /></form><div id="colorpicker"></div><input type="range" id="timeline" min="0" max="10"></input><div id="gyc-save-confirm" title="Confirm save"><form><label>Tags:<input type="text" id="drawingTags" placeholder="tag, tag2 ..."></input></label></form></div></div></div></div>');
+// This is injecting all the html we need to create the toolbar and the form to save to the website 
 
-$('#toolbar').accordion({
-    header: "#toolbar-toggle",
-    collapsible: true,
-    heightStyle: 'content',
-    active: false
-});
+function injectToolbar() {
+  $('body').append('<div class="getyourcrayon-menubar"><div id="toolbar-header"></div><div id="toolbar"><div id="toolbar-toggle"><p id="toggle-toolbar-arrow">&#9660</p></div><div id="toolbar-tools"><button type="gyc-button" id="gyc-paint-button" class="gyc-btn gyc-btn-default">Paint</button><button type="gyc-button" id="gyc-undo-button" class="gyc-btn gyc-btn-default">Undo</button><button type="gyc-button" id="gyc-save-button" class="gyc-btn gyc-btn-default">Save</button><label>Width: <span id="current_width">5</span><br><input id="width" type="range" name="points" min="1" max="40" value="5"></label><br><label>Opacity: <span id="current_opacity">100%</span><br><input id="opacity" type="range" name="points" min="1" max="100" value="100"></label><form><input type="text" id="color" name="color" value="#123456" /></form><div id="colorpicker"></div><input type="range" id="timeline" min="0" max="10"></input><div id="gyc-save-confirm" title="Confirm save"><form><label>Tags:<input type="text" id="drawingTags" placeholder="tag, tag2 ..."></input></label></form></div></div></div></div>');
+}
 
-var pscriptUrl = chrome.extension.getURL("drawController.js");
+// This injects the drawController.js script onto the page.  Note - the script must be of
+// type 'text/paperscript' attached to a specific canvas id for paper.js to function properly
 
-$('body').append('<script type="text/paperscript" src="'+ pscriptUrl + '" canvas="myCanvas"></script><canvas id="myCanvas" style="display:none;" resize></canvas>');
+function injectScripts() {
+  var pscriptUrl = chrome.extension.getURL("drawController.js");
+  $('body').append('<script type="text/paperscript" src="'+ pscriptUrl + '" canvas="myCanvas"></script><canvas id="myCanvas" style="display:none;" resize></canvas>');
+}
 
+// This listens for messages from the background script (background.js) and toggles the toolbar
+// on and off if the correct message is received.
 
+function initializeMessageListener(){
+  chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(request);
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.task == "toggle") {
+        sendResponse({status: "toggled"});
+        $('.getyourcrayon-menubar').toggle();
+      }
+    });
+}
 
-$('#gyc-paint-button').click(function(){
-  $('#myCanvas').toggle();
-});
+// This initializes the toolbar to have the accordion functionality once it is loaded and 
+// makes the entire toolbar draggable with the header specified as the handle for dragging
 
-// var data = {
-//   url: document.url,
-//   json_string: "sheeeeeeit"
-// }
-
-var windowUrl = window.location.href;
-
-
-
-
-$('#colorpicker').farbtastic('#color');
-
-$('.getyourcrayon-menubar').draggable({
-  handle: '#toolbar-header'
-});
-
-
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(request);
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello") {
-      sendResponse({farewell: "goodbye"});
-      $('.getyourcrayon-menubar').toggle();
-    }
+function initializeAccordion() {
+  $('#toolbar').accordion({
+      header: "#toolbar-toggle",
+      collapsible: true,
+      heightStyle: 'content',
+      active: false
   });
+}
+
+function initializeDraggable() {
+  $('.getyourcrayon-menubar').draggable({
+    handle: '#toolbar-header'
+  });
+}
+
+
+// This initializes the color picker on the toolbar through farbtastic.js
+
+function initializeColorPicker(){
+  $('#colorpicker').farbtastic('#color');
+}
+
+
 
