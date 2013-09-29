1d7faaa43f4ea7eeb0bc6aa6e001e7eaa11256fd
Apprentice <apprentice@dbc-chicago-001.local>
2013-09-29 13:45:26 -0500

    cleaned backgroud js

--- background.js
+++ background.js
@@ -1,11 +1,10 @@
-// console.log(chrome.browserAction.onClicked);
-// $(document).ready(function() {
+// This listens for a click on the browser action (chrome icon) and sends a message to the
+// content script instructing it to toggle the toolbar between hide() and show()
 
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(response) {
-      var msg = response ? 'success' : 'FAIL';
-      console.log(msg);
-
-    });
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.sendMessage(tab.id, {task: "toggle"}, function(response) {
+    var msg = response ? 'success' : 'FAIL';
+    console.log(msg);
   });
-// });
+});
+
