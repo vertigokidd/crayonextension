e2368685716ac85033637b9d40acdb067f5a45c1
Johnathon Francis <johnathon.t.francis@gmail.com>
2013-10-29 18:25:54 -0500

    minor tweaks

--- drawController.js
+++ drawController.js
@@ -357,7 +357,7 @@ Graffiti.prototype.saveDrawingPost = function(){
         $('#gyc-drawingTags').show();
         $('#gyc-drawingTags').prop('placeholder', 'Save Successful!');
         $('.gyc-random-class').text('Save Drawing');
-        $('.gyc-random-class').hide();
+        $('.gyc-random-class').hide('Save Drawing');
         setTimeout(function() {
           $('#gyc-drawingTags').css('box-shadow', 'none');
           graffitiView.toggleSaveButton();
